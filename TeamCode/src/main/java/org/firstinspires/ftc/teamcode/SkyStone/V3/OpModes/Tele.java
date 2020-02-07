1e6708bac57b7c55c4c7126ca276cc65edfe193c
Adam Rohrer <>
2020-02-06 22:13:28 -0500

    Added Tele program (Same as V2)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/V3/OpModes/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/V3/OpModes/Tele.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode.SkyStone.V3.OpModes;
 
-public class Tele {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.SkyStone.V2.Subsystems.Robot;
+
+@TeleOp(name = "Tele")
+public class Tele extends OpMode {
+    Robot robot;
+    ElapsedTime time;
+
+    @Override
+    public void init() {
+        robot = new Robot(this);//constructs robot and gives access to opmode
+        robot.mecanumDrive.runUsingEncoder(false);
+        time = new ElapsedTime();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("TIME", time.milliseconds());
+        robot.update();//updates all subsystems
+    }
+
+    @Override
+    public void stop() {
+        robot.opModeIsActive = false;
+        super.stop();
+    }
 }

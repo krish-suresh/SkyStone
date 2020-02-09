d7797cd183e6faaed8e26258ce4ae9a9f2510bca
Adam Rohrer <>
2020-02-09 17:03:38 -0500

    Refactored Auto to be more OOP and encapsulated, started on Auto body

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/V3/OpModes/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/V3/OpModes/Auto.java
@@ -2,11 +2,12 @@
 
 package org.firstinspires.ftc.teamcode.SkyStone.V3.OpModes;
 
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.RobotLibs.StickyGamepad;
+import org.firstinspires.ftc.teamcode.SkyStone.V3.Subsystems.Camera;
 import org.firstinspires.ftc.teamcode.SkyStone.V3.Subsystems.Robot;
 
 @Autonomous (name = "Auto")
@@ -14,26 +15,31 @@ public class Auto extends OpMode {
 
     // Start object/value creation
     Robot robot;
+    Camera camera;
 
     boolean tempUp = true;      // used for waitTime setting
     boolean tempDown = true;
 
     int skystone;       // from Camera, which position (0, 1, 2) the skystone is
 
-    AllianceColor allianceColor;
+    boolean allianceColorIsRed;
+
 
     double waitTime = 0;
 
-    AutoStates state = AutoStates.WAIT;
+    AutoState oopState;
+
+    ElapsedTime time;
     //End object/value creation
 
 
 
-    ElapsedTime time;
     @Override
     public void init() {
+        camera = new Camera(this);
         time = new ElapsedTime();
 
+        oopState = new Wait();
         // Initializations
 
     }
@@ -41,8 +47,8 @@ public class Auto extends OpMode {
     @Override
     public void init_loop() {
         updateWaitTime();       // increase / decrease wait time with GP1's dpad up and dpad down
-
-        allianceColor = (robot.stickyGamepad1.x ? AllianceColor.RED : AllianceColor.BLUE);      // toggle between alliance colors with GP1's x button
+        updateAllianceColor();  // flip allianceColor based on gamepad1.x
+        skystone = camera.getSkyPos(allianceColorIsRed);
 
         // Vision
 
@@ -94,34 +100,167 @@ public class Auto extends OpMode {
         *
         * */
 
-        switch(state) {
 
-            case WAIT:
-                break;
-            case WALL_TO_FIRST_BLOCK:
-                break;
-            case GRAB:
-                break;
-            case FIRST_STONE_TO_FOUNDATION:
-                break;
-            case MOVE_FOUNDATION_1:
-                break;
-            case FOUNDATION_TO_STONES:
-                break;
-            case STONES_TO_FOUNDATION:
-                break;
-            case PLACE_STONE:
-                break;
-            case MOVE_FOUNDATION_2:
-                break;
-            case PARK:
-                break;
-            case IDLE:
-                break;
+
+        // OOP auto
+        oopState = oopState.doLoop();
+        robot.telemetry.addLine("Current State is " + oopState.getStateName());
+        robot.telemetry.update();
+
+    }
+
+
+
+    // base class for all AutoState classes such as Wait, Grab
+    private abstract class AutoState {
+
+        /**
+         * Does methods of subclasses
+         * @return what state to run next loop
+         */
+        public abstract AutoState doLoop();
+
+        /**
+         * Used for telemetry of which state the robot is in
+         * @return the name of the current state
+         */
+        public String getStateName() {
+            return this.getClass().getName();
+        }
+
+    }
+
+
+    // wait a specified time before starting Auto
+    private class Wait extends AutoState {
+
+        @Override
+        public AutoState doLoop() {
+
+            if (time.seconds() < waitTime) {
+                return this;
+            } else {
+                return WALL_TO_FIRST_BLOCK;
+            }
+
+        }
+    }
+
+
+    private WallToFirstBlock WALL_TO_FIRST_BLOCK = new WallToFirstBlock();
+    private class WallToFirstBlock extends AutoState {
+
+        private WallToFirstBlock() {
+            robot.mecanumDrive.follower.followTrajectory(getTrajectory());
+        }
+
+        @Override
+        public AutoState doLoop() {
+            if (hasArrived()) {
+                return GRAB;
+            } else {
+                return this;
+            }
+        }
+
+        private Trajectory getTrajectory() {
+            return null;
+        }
+
+    }
+
+
+    private Grab GRAB = new Grab();
+    private class Grab extends AutoState {
+
+        private double grabTime = 0.6;
+
+        @Override
+        public AutoState doLoop() {
+            if (time.seconds() < grabTime) {
+                grabBlock();
+                return this;
+            } else {
+                return STONES_TO_FOUNDATION_1;
+            }
+        }
+
+        private void grabBlock() {
 
         }
 
+    }
 
+
+    private StonesToFoundation1 STONES_TO_FOUNDATION_1 = new StonesToFoundation1();
+    private class StonesToFoundation1 extends AutoState {
+
+        public StonesToFoundation1() {
+            robot.mecanumDrive.follower.followTrajectory(getTrajectory());
+        }
+
+        @Override
+        public AutoState doLoop() {
+            if (hasArrived()) {
+                return MOVE_FOUNDATION_1;
+            } else {
+                return this;
+            }
+        }
+
+        public Trajectory getTrajectory() {
+            return null;
+        }
+    }
+
+
+    private MoveFoundation1 MOVE_FOUNDATION_1 = new MoveFoundation1();
+    private class MoveFoundation1 extends AutoState {
+
+        private MoveFoundation1() {
+            robot.mecanumDrive.follower.followTrajectory(getTrajectory());
+        }
+
+        @Override
+        public AutoState doLoop() {
+            if (hasArrived()) {
+                return null;
+            } else {
+                return this;
+            }
+        }
+
+        private Trajectory getTrajectory() {
+            return null;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private PlaceStone PLACE_STONE = new PlaceStone();
+    private class PlaceStone extends AutoState {
+
+        @Override
+        public AutoState doLoop() {
+            return null;
+        }
+    }
+
+
+    private boolean hasArrived() {
+        return !robot.mecanumDrive.follower.isFollowing();
     }
 
     public void updateWaitTime() {
@@ -135,22 +274,24 @@ public class Auto extends OpMode {
         }
     }
 
-    public enum AutoStates {
-        WAIT,                           // wait a specified time before starting Auto
-        WALL_TO_FIRST_BLOCK,            // path from wall to the skystone detected closest to the bridge
-        GRAB,                           // grab the block in front of the robot (no xy movement)
-        FIRST_STONE_TO_FOUNDATION,      // path from the first stone to the foundation starting position
-        MOVE_FOUNDATION_1,              // move the foundation from starting position to close to the bridge
-        FOUNDATION_TO_STONES,           // path from foundation to next stone
-        STONES_TO_FOUNDATION,           // path from stones to foundation by bridge
-        PLACE_STONE,                    // place a stone on the foundation (no xy movement)
-        MOVE_FOUNDATION_2,              // move the foundation from close to the bridge back, turn, and push against wall
-        PARK,                           // activate scissor park once the foundation is in place
-        IDLE                            // end phase once robot is parked - does nothing
-
+    // flip allianceColor based on gamepad1.x
+    private void updateAllianceColor() {
+        allianceColorIsRed = robot.stickyGamepad1.x;
     }
 
-    public enum AllianceColor {
-        RED, BLUE
-    }
+
+//    WAIT,                           // wait a specified time before starting Auto
+//    WALL_TO_FIRST_BLOCK,            // path from wall to the skystone detected closest to the bridge
+//    GRAB,                           // grab the block in front of the robot (no xy movement)
+//    FIRST_STONE_TO_FOUNDATION,      // path from the first stone to the foundation starting position
+//    MOVE_FOUNDATION_1,              // move the foundation from starting position to close to the bridge
+//    FOUNDATION_TO_STONES,           // path from foundation to next stone
+//    STONES_TO_FOUNDATION,           // path from stones to foundation by bridge
+//    PLACE_STONE,                    // place a stone on the foundation (no xy movement)
+//    MOVE_FOUNDATION_2,              // move the foundation from close to the bridge back, turn, and push against wall
+//    PARK,                           // activate scissor park once the foundation is in place
+//    IDLE                            // end phase once robot is parked - does nothing
+
+
+
 }
